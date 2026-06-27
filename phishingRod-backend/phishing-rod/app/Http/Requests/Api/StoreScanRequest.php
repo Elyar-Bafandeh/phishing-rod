3bb153fde358e8f1e3cae159057303f4ab9f1be5
Elyar-Bafandeh <bafandehelyar@gmail.com>
2026-06-27 19:15:06 +0300

    add docs

--- phishingRod-backend/phishing-rod/app/Http/Requests/Api/StoreScanRequest.php
+++ phishingRod-backend/phishing-rod/app/Http/Requests/Api/StoreScanRequest.php
@@ -7,23 +7,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreScanRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'url' => [
+                'required',
+                'string',
+                'url',
+                'starts_with:http://,https://',
+                'max:2048',
+            ],
         ];
     }
 }
